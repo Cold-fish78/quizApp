43e22c96a09060f3c2a4e22f7f6a394f3ee66088
coldfish <pubg69089@gmail.com>
2022-01-19 18:22:24 +0530

    changed the app from stateless to stateful

--- lib/main.dart
+++ lib/main.dart
@@ -1,43 +1,56 @@
 import 'package:flutter/material.dart';
 
 void main() {
-  runApp(const Myapp());
+  return runApp(
+    MaterialApp(
+      home: Scaffold(
+        backgroundColor: Colors.red,
+        appBar: AppBar(
+          title: const Text('Dice'),
+          backgroundColor: Colors.red,
+        ),
+        body: const DicePage(),
+      ),
+    ),
+  );
 }
 
-class Myapp extends StatelessWidget {
-  const Myapp({Key? key}) : super(key: key);
+class DicePage extends StatefulWidget {
+  const DicePage({Key? key}) : super(key: key);
 
   @override
+  _DicePageState createState() => _DicePageState();
+}
+
+class _DicePageState extends State<DicePage> {
+  int leftDiceNumber = 5;
+  @override
   Widget build(BuildContext context) {
-    return MaterialApp(
-      home: Scaffold(
-        backgroundColor: Colors.teal,
-        body: SafeArea(
-          child: Center(
-            child: Row(
-              children: [
-                Expanded(
-                  flex: 2,
-                  child: FlatButton(
-                    onPressed : (){
-                      print('left button got pressed');
-                    },
-                    child: Image.asset('images/dice1.png'),
-                  ),
-                ),
-                Expanded(
-                  flex: 2,
-                  child: FlatButton(
-                    onPressed : (){
-                      print('left button got pressed');
-                    },
-                    child: Image.asset('images/dice2.png'),
-                  ),
-                ),
-              ],
+    return Center(
+      child: Row(
+        children: [
+          Expanded(
+            flex: 2,
+            child: FlatButton(
+              onPressed: () {
+                setState(() {
+                  leftDiceNumber = 3;
+                });
+
+              },
+              child: Image.asset('images/dice$leftDiceNumber.png'),
             ),
           ),
-         ),
+          Expanded(
+            flex: 2,
+            child: FlatButton(
+              onPressed: () {
+                print('left button got pressed');
+              },
+              child: Image.asset('images/dice2.png'),
+            ),
+          ),
+        ],
       ),
     );
   }
